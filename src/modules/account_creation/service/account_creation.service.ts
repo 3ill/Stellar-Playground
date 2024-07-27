7c692a2c1063a0d5814ac7d29ffe6633f0d5dc2a
3illBaby <thrillanon@outlook.com>
2024-07-27 23:10:56 +0100

    feat(account_creation.service.ts):impl func to create keyPair and get balances

--- src/modules/account_creation/service/account_creation.service.ts
+++ src/modules/account_creation/service/account_creation.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { AccountCreation } from '../provider/account_creation';
+import {
+  AccountBalanceInterface,
+  StellarNetworkType,
+} from '../interface/account_creation.interface';
 
 @Injectable()
-export class AccountCreationService {}
+export class AccountCreationService {
+  constructor(private readonly provider: AccountCreation) {}
+
+  createKeyPair(network: StellarNetworkType) {
+    try {
+      const account = this.provider.provideAccount(network);
+      const keyPair = account.createKeypair();
+      if (!keyPair) {
+        return {
+          status: HttpStatus.BAD_REQUEST,
+          message: 'Could not create keypair',
+        };
+      }
+
+      const publicKey = keyPair.publicKey;
+      const secretKey = keyPair.secretKey;
+
+      const keyObject = {
+        publicKey,
+        secretKey,
+      };
+
+      return {
+        status: HttpStatus.OK,
+        keyObject,
+        data: keyPair,
+        message: 'Keypair created successfully',
+      };
+    } catch (error) {
+      console.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async getAccountBalance(args: AccountBalanceInterface) {
+    const { network, publicKey } = args;
+    try {
+      const server = this.provider.provideStellarServer(network).server;
+      const account = server.loadAccount(publicKey);
+      const balance = (await account).balances;
+      return balance;
+    } catch (error) {
+      error;
+      console.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
