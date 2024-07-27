882a2ea072f6dc897eedb56e8a3b85442e61805d
3illBaby <thrillanon@outlook.com>
2024-07-27 22:13:09 +0100

    chore(account_creation.interface.ts):added anchor interfaces

--- src/modules/account_creation/interface/account_creation.interface.ts
+++ src/modules/account_creation/interface/account_creation.interface.ts
@@ -1,1 +1,13 @@
 export type StellarNetworkType = 'testnet' | 'mainnet';
+
+export enum AnchorDomains {
+  DEFAULT = 'testanchor.stellar.org',
+}
+
+export interface AnchorDomainInterface {
+  domain?: string;
+}
+
+export interface AnchorProviderInterface extends AnchorDomainInterface {
+  network: StellarNetworkType;
+}
