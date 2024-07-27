d4ae402c7dcd3fc71c7f98458cbdd2c20ab1ebc2
3illBaby <thrillanon@outlook.com>
2024-07-27 23:11:37 +0100

    test(account_creation.service.spec.ts):tested func to create keyPair

--- src/modules/account_creation/service/account_creation.service.spec.ts
+++ src/modules/account_creation/service/account_creation.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountCreationService } from './account_creation.service';
+import { AccountCreation } from '../provider/account_creation';
 
 describe('AccountCreationService', () => {
   let service: AccountCreationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AccountCreationService],
+      providers: [AccountCreationService, AccountCreation],
     }).compile();
 
     service = module.get<AccountCreationService>(AccountCreationService);
@@ -14,5 +15,18 @@ describe('AccountCreationService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('KeyPairs', () => {
+    it('Should successfully create a keypair', async () => {
+      const keyPair = service.createKeyPair('testnet');
+      console.log(keyPair);
+      expect(keyPair).toBeDefined();
+      expect(keyPair.status).toBe(200);
+      expect(keyPair.message).toBe('Keypair created successfully');
+      expect(keyPair.keyObject).toBeDefined();
+      expect(keyPair.keyObject.publicKey).toBeDefined();
+      expect(keyPair.keyObject.secretKey).toBeDefined();
+    });
   });
 });
