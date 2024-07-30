be61db8c04b53bfa26cb010edf86465be7a7e2ee
3illBaby <thrillanon@outlook.com>
2024-07-30 10:46:21 +0100

    chore(authentication.module.ts): imported controller

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationProvider } from './provider/authentication.provider';
 import { AuthenticationService } from './service/authentication.service';
+import { AuthenticationController } from './controller/authentication.controller';
 
 @Module({
   providers: [AuthenticationService, AuthenticationProvider],
+  controllers: [AuthenticationController],
 })
 export class AuthenticationModule {}
