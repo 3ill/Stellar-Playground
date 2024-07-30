b7ba6a434bbf117f25a93b470108032aedbad142
3illBaby <thrillanon@outlook.com>
2024-07-30 11:02:12 +0100

    chore(authentication.module.ts):imported account creation module

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthenticationProvider } from './provider/authentication.provider';
 import { AuthenticationService } from './service/authentication.service';
 import { AuthenticationController } from './controller/authentication.controller';
+import { AccountCreationModule } from '../account_creation/account_creation.module';
 
 @Module({
+  imports: [AccountCreationModule],
   providers: [AuthenticationService, AuthenticationProvider],
   controllers: [AuthenticationController],
 })
