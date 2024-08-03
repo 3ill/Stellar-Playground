aeed8aa26d28da64093cdbddcdc1c9b58d7182e6
3illBaby <thrillanon@outlook.com>
2024-08-03 22:31:13 +0100

    feat(authentication.service.ts):impl func to derive an auth token

--- src/modules/authentication/service/authentication.service.ts
+++ src/modules/authentication/service/authentication.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { AccountCreationProvider } from '@/modules/account_creation/provider/account_creation.provider';
+import { AccountCreationService } from '@/modules/account_creation/service/account_creation.service';
+import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    private readonly accountCreationService: AccountCreationService,
+    private readonly accountCreationProvider: AccountCreationProvider,
+  ) {}
+
+  async authenticate() {
+    const keyPair = this.accountCreationService.createKeyPair('testnet');
+
+    if (!keyPair) {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Could not create keypair',
+      };
+    }
+    const keyObjects = keyPair.keyObject;
+    if (!keyObjects) {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Could not create keypair',
+      };
+    }
+    const anchor = this.accountCreationProvider.provideAnchor({
+      network: 'testnet',
+    });
+
+    const sep10 = await anchor.sep10();
+    const authToken = await sep10.authenticate({ accountKp: keyPair.data });
+    if (!authToken) {
+      return {
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Could not authenticate',
+      };
+    }
+    return {
+      status: HttpStatus.OK,
+      message: 'Authentication successful',
+      data: authToken,
+      keys: keyObjects,
+    };
+  }
+}
