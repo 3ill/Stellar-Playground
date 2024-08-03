404f4d328e47c732f0c1657524b0db2f3c11de74
3illBaby <thrillanon@outlook.com>
2024-08-03 22:31:56 +0100

    test(authentication.service.spec.ts):tested func to derive auth token

--- src/modules/authentication/service/authentication.service.spec.ts
+++ src/modules/authentication/service/authentication.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
+import { AccountCreationModule } from '@/modules/account_creation/account_creation.module';
+import { AccountCreationProvider } from '@/modules/account_creation/provider/account_creation.provider';
+import { AccountCreationService } from '@/modules/account_creation/service/account_creation.service';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
+      imports: [AccountCreationModule],
+      providers: [
+        AuthenticationService,
+        AccountCreationProvider,
+        AccountCreationService,
+      ],
     }).compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
@@ -14,5 +22,13 @@ describe('AuthenticationService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Authentication', () => {
+    it('should return an authentication token', async () => {
+      const data = await service.authenticate();
+      console.log(data);
+      expect(data).toBeDefined();
+    });
   });
 });
