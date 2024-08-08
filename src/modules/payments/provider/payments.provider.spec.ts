bd6c881fcd11fe2c22f08db62b75019a6e6380d0
3illBaby <thrillanon@outlook.com>
2024-08-08 18:33:40 +0100

    test(payments.provider.spec.ts):tested func to list anchor asset

--- src/modules/payments/provider/payments.provider.spec.ts
+++ src/modules/payments/provider/payments.provider.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentsProvider } from './payments.provider';
+import { AccountCreationModule } from '@/modules/account_creation/account_creation.module';
+import { AccountCreationProvider } from '@/modules/account_creation/provider/account_creation.provider';
 
 describe('Payments', () => {
   let provider: PaymentsProvider;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PaymentsProvider],
+      imports: [AccountCreationModule],
+      providers: [PaymentsProvider, AccountCreationProvider],
     }).compile();
 
     provider = module.get<PaymentsProvider>(PaymentsProvider);
@@ -14,5 +17,23 @@ describe('Payments', () => {
 
   it('should be defined', () => {
     expect(provider).toBeDefined();
+  });
+
+  describe('Asset', () => {
+    it('Should list all anchor assets', async () => {
+      const currencies = await provider.listAnchorCurrencies('testnet');
+      console.log(currencies);
+      expect(currencies).toBeDefined();
+    });
+
+    it.only('Should get asset code and issuer public key', async () => {
+      const assetConfig = await provider.getAssetConfig({
+        asset: 'SRT',
+        network: 'testnet',
+      });
+
+      console.log(assetConfig);
+      expect(assetConfig).toBeDefined();
+    });
   });
 });
