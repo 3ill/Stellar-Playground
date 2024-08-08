0c5bd8d23146875b386e3f0b2053bfb980fbd346
3illBaby <thrillanon@outlook.com>
2024-08-08 18:31:39 +0100

    chore(payments.interface.ts): created interfaces

--- src/modules/payments/interface/payments.interface.ts
+++ src/modules/payments/interface/payments.interface.ts
@@ -1,1 +1,11 @@
-export interface Payments {}
+import { StellarNetworkType } from '@/modules/account_creation/interface/account_creation.interface';
+
+export interface GetAssetConfigInterface {
+  network: StellarNetworkType;
+  asset: string;
+  domain?: string;
+}
+
+export interface TrustInterface extends GetAssetConfigInterface {
+  privateKey: string;
+}
